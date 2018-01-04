3f8ef7cc1d6a7338fdf11bc63df913aeeace5577
theapprenticewizard <rayd3900@gmai.com>
2018-01-03 21:27:00 -0500

    add hamcrest dependency

--- src/test/java/OptionalTest.java
+++ src/test/java/OptionalTest.java
@@ -1,8 +1,9 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class OptionalTest {
 
@@ -10,5 +11,30 @@ public class OptionalTest {
     public void testEmptyOptionalCreation() {
         Optional<String> optional = Optional.empty();
         assertFalse(optional.isPresent());
+        assertThrows(NoSuchElementException.class, optional::get);
     }
+
+    @Test
+    public void testCreateOptionalWithOf() {
+        Optional<String> optional = Optional.of("foo");
+        assertEquals("foo", optional.get());
+    }
+
+    @Test
+    public void testCreateOptionalWithOfThrowsExceptionWhenNull() {
+        try {
+            Optional<String> optional = null;
+            System.out.println(optional.get());
+            fail("test failed");
+        } catch (NullPointerException ex) {}
+    }
+
+    @Test
+    public void testCreateOptionalWithNullable() {
+        Optional<Integer> integer = Optional.ofNullable(null);
+        NoSuchElementException noSuchElementException = assertThrows(
+                NoSuchElementException.class, integer::get
+        );
+    }
+
 }
