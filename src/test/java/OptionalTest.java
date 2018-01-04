8f2217bd1ec0c765397b4a5f0e43ba817058abb6
theapprenticewizard <rayd3900@gmai.com>
2018-01-03 20:45:53 -0500

    specify junit5

--- src/test/java/OptionalTest.java
+++ src/test/java/OptionalTest.java
@@ -1,2 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 public class OptionalTest {
+
+    @Test
+    public void testEmptyOptionalCreation() {
+        Optional<String> optional = Optional.empty();
+        assertFalse(optional.isPresent());
+    }
 }
